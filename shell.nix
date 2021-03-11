df02b6aa81e7bd4418988894983cc22e129b9477
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-03-11 15:56:33 +0800

    Setting up the environment for ruby with nix & direnv!

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # See https://nixos.wiki/wiki/Development_environment_with_nix-shell for a quickstart on setting up virtual environments with nix and direnv
+
+    # EDIT: Turns out you don't need to prepend the package name with pkg.buildPackages.<package>
+    # Also, to find out what packages are available for use, https://search.nixos.org
+    nativeBuildInputs = [ pkgs.ruby ];
+}
