8b5ae262bfd278b709554fce0a1e2576ab16bdda
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-03-20 00:16:35 +0800

    erhmagawd bubblesort

--- prolog/bubble.pl
+++ prolog/bubble.pl
@@ -1,11 +1,12 @@
-bubblesort([H|T],Sorted) :-
-    bubblesort([H|T],[H], Sorted).
+bubblesort([H1,H2|T],Sorted) :-
+    do_bsort([H2|T],[H1], Sorted).
 
-bubblesort([],SL,SL).
-bubblesort([H|T],[H2|T2],SL) :-
+do_bsort([],SL,SL).
+do_bsort(SL,[],SL).
+do_bsort([H|T],[H2|T2],SL) :-
     H =< H2,
-    bubblesort(T,[H,H2|T2],SL).
-bubblesort([H|T],[H2|T2],SL) :-
+    do_bsort(T,[H,H2|T2],SL).
+do_bsort([H|T],[H2|T2],SL) :-
     H > H2,
-    bubblesort([H],T2,TN),
-    bubblesort(T,[H2|TN],SL).
+    do_bsort([H],T2,TN), % bruh im stuck
+    do_bsort(T,[H2|TN],SL).
