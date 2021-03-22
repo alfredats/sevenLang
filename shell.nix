60b9b6899729fe675f96f14f8e5df1e71bb51a49
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-03-22 22:29:00 +0800

    Added openJDK to shell.nix for Alloy env

--- shell.nix
+++ shell.nix
@@ -4,5 +4,5 @@
 
     # EDIT: Turns out you don't need to prepend the package name with pkg.buildPackages.<package>
     # Also, to find out what packages are available for use, https://search.nixos.org
-    nativeBuildInputs = [ pkgs.ruby pkgs.swiProlog ];
+    nativeBuildInputs = [ pkgs.ruby pkgs.swiProlog pkgs.openjdk11 ];
 }
